603ead28bd670c48663512088951a001fb5b20ff
RXNationGaming <77547237+RXNationGMG@users.noreply.github.com>
2021-11-03 11:39:29 +0800

    👍👍👍

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,6 +1,6 @@
 const { PREFIX } = require('../../config.js');
 module.exports = async bot => {
-    console.log(`${bot.user.username} is available now!`)
+    console.log(`${bot.user.username} Is Now Online!`)
     let totalUsers = bot.guilds.cache.reduce((acc, value) => acc + value.memberCount, 0)
     var activities = [ `${bot.guilds.cache.size} servers`, `${totalUsers} users!`, `https://rxnationgaming.cf` ], i = 0;
     setInterval(() => bot.user.setActivity(`${PREFIX}help | ${activities[i++ % activities.length]}`, { type: "PLAYING" }),5000)
