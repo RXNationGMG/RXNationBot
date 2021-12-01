80ce1b2f3f39a1942de6f9862af71c22a9044c51
RXNationGaming <77547237+RXNationGMG@users.noreply.github.com>
2021-12-01 21:42:32 +0800

    ...

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 //Here Is The setup & paste ur token in here tnks :)
 
-exports.TOKEN = "YOURTOKEN";
+exports.TOKEN = "YOUR DISCORD BOT TOKEN";
 
-exports.DBL_API_KEY = "NONE";
+exports.DBL_API_KEY = "YOUR DBL API KEY";
 
 exports.PREFIX = "rx!";
 
-exports.GOOGLE_API_KEY = "AIzaSyDzKnUyDiDaZru7_zbAkPbIdqhhnjLvd8U";
+exports.GOOGLE_API_KEY = "AIzaSyDzKnUyDiDaZru7_zbAkPbIdqhhnjLvd8U"; //No Need To Set It Up
 
-exports.GENIUS_API_KEY = "VE-Hkn049y3FN2oYctEIL1Kc6eCaORIrNNaPXKqihSHTJqWjnvh4FFm67Upgo5Jj";
+exports.GENIUS_API_KEY = "VE-Hkn049y3FN2oYctEIL1Kc6eCaORIrNNaPXKqihSHTJqWjnvh4FFm67Upgo5Jj"; //No Need To Set It Up
 
-exports.yandex_API = "trnsl.1.1.20200524T161905Z.bfe2aa5a35ce8bd9.2e55fde04fbce222eb14506eba89f1dbfcfc973d";
+exports.yandex_API = "trnsl.1.1.20200524T161905Z.bfe2aa5a35ce8bd9.2e55fde04fbce222eb14506eba89f1dbfcfc973d"; //No Need To Set It Up
 
-exports.news_API = "ac620cf6a0aa49b398f8f071edcaa71f";
+exports.news_API = "ac620cf6a0aa49b398f8f071edcaa71f"; //No Need To Set It Up
 
-exports.giphy_API = "RmKm0HixW7lQ6F8cTSQpyWaj0bjT7laH";
+exports.giphy_API = "RmKm0HixW7lQ6F8cTSQpyWaj0bjT7laH"; //No Need To Set It Up
 
-exports.AME_API = "a0f7f1b61e152b375952b83d2b6a7ae64c32fdc2fcc7a4f6b6963dc4c447731745c451adabde6a070731bb99739c68350668074b7fcec856e8222a75ab7da555";
+exports.AME_API = "a0f7f1b61e152b375952b83d2b6a7ae64c32fdc2fcc7a4f6b6963dc4c447731745c451adabde6a070731bb99739c68350668074b7fcec856e8222a75ab7da555"; //No Need To Set It Up
 
-exports.blague_API = "r.00yqXn3KtkY41GNdGCB._JbaMPdqyAyLA6JLkCjftATM5QXTvdatalXNzvcBR-";
+exports.blague_API = "r.00yqXn3KtkY41GNdGCB._JbaMPdqyAyLA6JLkCjftATM5QXTvdatalXNzvcBR-"; //No Need To Set It Up
