73dc718bb104238794ee06bc3afb4988b47f332f
RXNationGaming <77547237+RXNationGMG@users.noreply.github.com>
2021-12-04 15:19:13 +0800

    Update nekogif.js

--- commands/anime/nekogif.js
+++ commands/anime/nekogif.js
@@ -15,7 +15,7 @@ module.exports = {
 
   let victim = message.mentions.users.first() || (args.length > 0 ? message.users.cache.filter(e => e.username.toLowerCase().includes(args.join(" ").toLowerCase())).first(): message.author) || message.author;
   const { body } = await superagent
-    .get("https://purrbot.site/img/sfw/neko/gif/neko_[Math.floor(Math.random().gif");
+    .get("https://purrbot.site/api/img/sfw/neko/gif");
         const embed = new MessageEmbed()
         .setColor("FF0000")
         .setTitle("Here's your Neko Gif 🤗")
